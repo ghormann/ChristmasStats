7df8a7e0bc5558744cedfea4220cd3bf714ad490
Greg Hormann <ghormann@gmail.com>
2019-11-22 21:27:20 -0500

    Added infexes to tables;

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS mqtt;
 USE mqtt;
-CREATE TABLE IF NOT EXISTS mqtt.name (name VARCHAR(255), name_type varchar(255), ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
-CREATE TABLE IF NOT EXISTS mqtt.event (name VARCHAR(255), argument varchar(255), ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
-CREATE TABLE IF NOT EXISTS mqtt.song (songid SMALLINT, source varchar(255), ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+CREATE TABLE IF NOT EXISTS mqtt.name (name VARCHAR(255), name_type varchar(255), ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, index name_ts_idx(ts));
+CREATE TABLE IF NOT EXISTS mqtt.event (name VARCHAR(255), argument varchar(255), ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, index event_ts_idx(ts));
+CREATE TABLE IF NOT EXISTS mqtt.song (songid SMALLINT, source varchar(255), ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, index song_ts_idx(ts));
