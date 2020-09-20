a7bac6b314a9c98f264aa81ca2deec6c1f6792ef
Greg Hormann <ghormann@gmail.com>
2020-09-20 19:07:53 -0400

    Ignore internal Songs for stats

--- app/lib/db.js
+++ app/lib/db.js
@@ -126,7 +126,7 @@ function getPowerToday() {
  */
 function getTopPlayedSongs(minutes) {
   let sql =
-    "select argument, count(1) CNT from event where name = 'planSong' and argument not in ('TuneTo','off','Intro','Good_Night', 'TheHormanns') and argument not like 'Test%' and argument not like 'Midnight%' and ts > now() - interval ? minute group by argument order by 2 desc LIMIT 20";
+    "select argument, count(1) CNT from event where name = 'planSong' and argument not in ('TuneTo','off','Intro','Good_Night', 'TheHormanns') and argument not like 'Test%' and argument not like 'Internal%' and argument not like 'Midnight%' and ts > now() - interval ? minute group by argument order by 2 desc LIMIT 20";
   return new Promise(function (resolve, reject) {
     pool.query(sql, [minutes], function (error, results, fields) {
       if (error) reject(error);
